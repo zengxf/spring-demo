cdc53207c91116bdafda2886fe8d2a97192380c0
zxfeng <fl_zxf@163.com>
2019-05-15 16:48:12 +0800

    Apollo 配置测试 namespace, cluster, environment

--- spring-configuration/apollo-configuration-test/src/main/java/cn/zxf/spring_research/ApolloConfiguration.java
+++ spring-configuration/apollo-configuration-test/src/main/java/cn/zxf/spring_research/ApolloConfiguration.java
@@ -1,8 +1,10 @@
 package cn.zxf.spring_research;
 
+import org.springframework.context.annotation.Configuration;
+
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 
-@EnableApolloConfig
+@Configuration
+@EnableApolloConfig( { "ns1", "ns2", "application.yml" } )
 public class ApolloConfiguration {
-
 }
--- spring-configuration/apollo-configuration-test/src/main/java/cn/zxf/spring_research/biz/BizController.java
+++ spring-configuration/apollo-configuration-test/src/main/java/cn/zxf/spring_research/biz/BizController.java
@@ -1,5 +1,6 @@
 package cn.zxf.spring_research.biz;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
 
 @RestController
 @RequestMapping( "/api/biz" )
@@ -16,22 +18,32 @@ public class BizController {
 
     @Value( "${test-test1:无}" )
     private String test1;
-
     @Value( "${ns1.name:无}" )
     private String ns1Name;
+    @Value( "${dev.name:无}" )
+    private String devName;
 
     // http://localhost:9001/api/biz/hello
     @GetMapping( "hello" )
     public String hello() {
-        return String.format( "hello-[%s]-[%s]", test1, ns1Name );
+        return String.format( "hello-[%s]-[%s]-[%s]", test1, ns1Name, devName );
     }
 
     // http://localhost:9001/api/biz/config
     @GetMapping( "config" )
     public Map<String, Object> config() {
-        Config config = ConfigService.getAppConfig();
-        Config ns1Config = ConfigService.getConfig( "ns1" );
-        return Map.of( "def", config.getPropertyNames(), "ns1", ns1Config.getPropertyNames() );
+        Map<String, Object> cM = toMap( ConfigService.getAppConfig() );
+        Map<String, Object> ns1M = toMap( ConfigService.getConfig( "ns1" ) );
+        String ns2Content = ConfigService.getConfigFile( "ns2", ConfigFileFormat.YAML )
+                .getContent();
+        return Map.of( "def", cM, "ns1", ns1M, "ns2Content", ns2Content );
+    }
+
+    Map<String, Object> toMap( Config config ) {
+        Map<String, Object> cM = new HashMap<>();
+        config.getPropertyNames()
+                .forEach( key -> cM.put( key, config.getProperty( key, "无" ) ) );
+        return cM;
     }
 
 }
