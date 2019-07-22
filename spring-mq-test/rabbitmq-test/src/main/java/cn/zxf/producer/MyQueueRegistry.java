f64db76406784b72161b5be570317f78334096df
feng <fl_zxf@sina.cn>
2019-07-22 12:43:55 +0800

    test delay msg

--- spring-mq-test/rabbitmq-test/src/main/java/cn/zxf/producer/MyQueueRegistry.java
+++ spring-mq-test/rabbitmq-test/src/main/java/cn/zxf/producer/MyQueueRegistry.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.Binding.DestinationType;
+import org.springframework.amqp.core.CustomExchange;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.BeansException;
@@ -25,6 +26,42 @@ public class MyQueueRegistry implements BeanDefinitionRegistryPostProcessor {
 
     @Override
     public void postProcessBeanDefinitionRegistry( BeanDefinitionRegistry registry ) throws BeansException {
+        // simple( registry );
+
+        String exName = "test-delay-zxf";
+        BeanDefinition exDef = BeanDefinitionBuilder.genericBeanDefinition( CustomExchange.class )
+                .addConstructorArgValue( exName )
+                .addConstructorArgValue( "x-delayed-message" ) // type
+                .addConstructorArgValue( true ) // durable
+                .addConstructorArgValue( false ) // autoDelete
+                .addConstructorArgValue( Map.of( "x-delayed-type", "direct" ) )
+                .getRawBeanDefinition();
+        registry.registerBeanDefinition( exName, exDef ); // 注册 Bean
+        System.out.println( "注册 CustomExchange：" + exName );
+
+        String queueName = "test-delay-queue";
+        BeanDefinition queueDef = BeanDefinitionBuilder.genericBeanDefinition( Queue.class )
+                .addConstructorArgValue( queueName )
+                .addConstructorArgValue( true )
+                .addConstructorArgValue( false )
+                .addConstructorArgValue( false )
+                .addConstructorArgValue( null ) // new HashMap<String, Object>()
+                .getRawBeanDefinition();
+        registry.registerBeanDefinition( queueName, queueDef ); // 注册 Bean
+        System.out.println( "注册 Queue：" + queueName );
+
+        BeanDefinition bindDef = BeanDefinitionBuilder.genericBeanDefinition( Binding.class )
+                .addConstructorArgValue( queueName )
+                .addConstructorArgValue( DestinationType.QUEUE )
+                .addConstructorArgValue( exName )
+                .addConstructorArgValue( "" )
+                .addConstructorArgValue( new HashMap<String, Object>() )
+                .getRawBeanDefinition();
+        registry.registerBeanDefinition( exName + "-" + queueName, bindDef ); // 注册 Bean
+        System.out.println( "注册 Binding：" + exName + "-" + queueName );
+    }
+
+    void simple( BeanDefinitionRegistry registry ) {
         List<String> queues = List.of( "test-list-1", "test-list-2", "test-list-3" );
         Map<String, List<String>> map = Map.of( "test-fanout-01", queues );
 
--- spring-mq-test/rabbitmq-test/src/main/java/cn/zxf/producer/biz/BizController.java
+++ spring-mq-test/rabbitmq-test/src/main/java/cn/zxf/producer/biz/BizController.java
@@ -1,10 +1,18 @@
 package cn.zxf.producer.biz;
 
-import org.springframework.amqp.core.AmqpTemplate;
+import java.io.IOException;
+import java.time.LocalTime;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cn.zxf.common.Email;
 
@@ -13,16 +21,56 @@ import cn.zxf.common.Email;
 public class BizController {
 
     @Autowired
-    AmqpTemplate rabbitTemplate;
+    RabbitTemplate rabbitTemplate;
 
     // http://localhost:9011/api/biz/hello
     @GetMapping( "hello" )
     public String hello() {
         Email message = new Email( "zxf@s.cn", "== test == " + System.currentTimeMillis() % 1000 );
         System.out.println( "send => " + message );
-        // rabbitTemplate.convertAndSend( "test-list-2", message );
-        rabbitTemplate.convertAndSend( "test-fanout-01", "", message );
+        rabbitTemplate.convertAndSend( "test-obj-002", message );
+        // rabbitTemplate.convertAndSend( "test-fanout-01", "", message );
         return "hello";
+    }
+
+    // http://localhost:9011/api/biz/test-delay
+    @Deprecated
+    @GetMapping( "test-delay" )
+    public String testDelay() {
+        Email body = new Email( "zxf@s.cn", "== test == " + System.currentTimeMillis() % 1000 );
+        System.out.println( "[" + LocalTime.now() + "] send => " + body );
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "";
+        try {
+            json = mapper.writeValueAsString( body );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        MessageProperties properties = new MessageProperties();
+        properties.setDelay( 5000 );
+
+        Message message = new Message( json.getBytes(), properties );
+        rabbitTemplate.send( "test-delay-zxf", "", message );
+        return "OK - " + json;
+    }
+
+    // http://localhost:9011/api/biz/test-delay-2
+    @GetMapping( "test-delay-2" )
+    public String testDelay2() {
+        Email body = new Email( "zxf@s.cn", "== test == " + System.currentTimeMillis() % 1000 );
+        MessageProperties properties = new MessageProperties();
+        properties.setDelay( 5000 );
+
+        MessagePostProcessor processor = msg -> {
+            return rabbitTemplate.getMessageConverter()
+                    .toMessage( body, properties );
+        };
+
+        rabbitTemplate.convertAndSend( "test-delay-zxf", "", body, processor );
+        System.out.println( "[" + LocalTime.now() + "] send => " + body );
+
+        return "OK - " + body;
     }
 
 }
