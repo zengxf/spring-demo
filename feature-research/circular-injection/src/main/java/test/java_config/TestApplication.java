9977c1ea434da27b738394d33c3c56570c965f90
feng <fl_zxf@sina.cn>
2019-10-23 17:01:28 +0800

    添加参考

--- feature-research/circular-injection/src/main/java/test/java_config/TestApplication.java
+++ feature-research/circular-injection/src/main/java/test/java_config/TestApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+/**
+ * 参考：<br>
+ * https://blog.mythsman.com/post/5d838c7c2db8a452e9b7082c/ <br>
+ * https://mp.weixin.qq.com/s?__biz=MzIwMDY0Nzk2Mw==&mid=2650320674&idx=1&sn=837fd13a64c7b472b564eb9bdc86044e
+ * <p>
+ * Created by zengxf on 2019-10-23
+ */
 @SpringBootApplication
 // @Import( { ConfigurationA.class, BeanB.class, } ) // OK
 // @Import( { ConfigurationA.class, BeanB.class, ServiceA.class, } ) // OK
