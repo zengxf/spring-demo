6998caa8a8ee40ade233f5cd4fe560e8159f8a43
ZXFeng <fa6688@sina.com>
2021-11-12 16:17:42 +0800

    Gradle 多模块

---
+++ 0-fast-spring-starter/gradle-multi-module/spring-web-1/src/main/java/test/MainApplication1.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication1.class, args);
+    }
+
+}
