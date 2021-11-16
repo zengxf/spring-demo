a1aa13dc0ffb90d23228ea3696567195d1f81468
ZXFeng <fa6688@sina.com>
2021-11-16 10:10:12 +0800

    Mock Data

--- web/mock-data-web/src/main/java/test/user/LoginController.java
+++ web/mock-data-web/src/main/java/test/user/LoginController.java
@@ -1,31 +1,41 @@
 package test.user;
 
 import cn.hutool.core.map.MapBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
  * <br/>
  * Created by ZXFeng on  2021/9/22.
  */
+@Slf4j
 @RestController
 public class LoginController {
 
-    static String TOKEN = "ok-123";
+    public static final String
+            TOKEN = "ok-123",
+            USERNAME = "zxf",
+            PASSWORD = "123";
 
     @PostMapping("/admin/login")
     public Map<String, Object> login(
-            @RequestBody Map<String, String> body
+            @RequestBody Map<String, String> body,
+            HttpServletResponse response
     ) {
         String username = body.get("username");
         String password = body.get("password");
 
-        if ("zxf".equalsIgnoreCase(username)
-                && "123".equalsIgnoreCase(password)) {
+        log.info("login, body: [{}], username: [{}], password: [{}]", body, username, password);
 
+        if (USERNAME.equalsIgnoreCase(username)
+                && PASSWORD.equalsIgnoreCase(password)) {
+            log.info("登录成功！");
+            response.addHeader("token", TOKEN);
             return MapBuilder.<String, Object>create()
                     .put("res", 1)
                     .put("msg", "登录成功")
@@ -33,6 +43,7 @@ public class LoginController {
                     .build();
         }
 
+        log.info("登录失败！");
         return MapBuilder.<String, Object>create()
                 .put("res", 0)
                 .put("msg", "账号或密码错误")
--- web/mock-data-web/src/main/java/test/user/UserController.java
+++ web/mock-data-web/src/main/java/test/user/UserController.java
@@ -1,6 +1,7 @@
 package test.user;
 
 import cn.hutool.core.map.MapBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
@@ -14,29 +15,39 @@ import java.util.stream.IntStream;
  * <br/>
  * Created by ZXFeng on  2021/9/22.
  */
+@Slf4j
 @RestController
 public class UserController {
 
     AtomicInteger idAdder = new AtomicInteger();
     Map<String, UserDto> map = new LinkedHashMap<>();
+    private static final String
+            ID_PREFIX = "id-";
 
     @PostMapping("user/create")
     public UserDto createUser(
             @RequestBody UserDto user
     ) {
-        user.setId("id-" + idAdder.incrementAndGet())
+        log.info("create, user: [{}]", user);
+
+        user.setId(ID_PREFIX + idAdder.incrementAndGet())
                 .setStatus("new");
         map.put(user.getId(), user);
+
         return user;
     }
 
     // http://localhost:9066/user/find-all
     @GetMapping("user/find-all")
-    public List<UserDto> findAll() {
+    public List<UserDto> findAll() throws Exception {
+        log.info("--------------");
         this.initData();
-        return map.values()
+        Thread.sleep(1000L);
+        List<UserDto> users = map.values()
                 .stream()
                 .collect(Collectors.toList());
+        users.forEach(user -> log.info("返回的 user: [ {} ]", user));
+        return users;
     }
 
     @GetMapping("user/find-one/{id}")
@@ -44,6 +55,7 @@ public class UserController {
             @PathVariable String id,
             @RequestHeader(required = false) String token
     ) {
+        log.info("find one, id: [{}], token: [{}]", id, token);
         if (!LoginController.TOKEN.equalsIgnoreCase(token)) {
             return new UserDto().setRemark("Token 无效");
         }
@@ -51,24 +63,12 @@ public class UserController {
         return map.get(id);
     }
 
-    private void initData() {
-        if (map.isEmpty()) {
-            IntStream.rangeClosed(1, 3)
-                    .mapToObj(i -> new UserDto()
-                            .setId("id-" + idAdder.incrementAndGet())
-                            .setName("test-" + i)
-                            .setAge(32 + i)
-                            .setRemark("系统创建")
-                            .setStatus("auto-new")
-                    )
-                    .forEach(dto -> map.put(dto.getId(), dto));
-        }
-    }
-
     @PutMapping("user/update")
     public Map<String, Object> updateUser(
             @RequestBody UserDto user
     ) {
+        log.info("update, user: [{}]", user);
+
         if (!map.containsKey(user.getId())) {
             return MapBuilder.<String, Object>create()
                     .put("res", 0)
@@ -85,10 +85,35 @@ public class UserController {
                 .build();
     }
 
+    @PostMapping("user/batch-update")
+    public Map<String, Object> batchUpdateUser(
+            @RequestBody List<UserDto> users
+    ) {
+        log.info("--------------");
+        users.forEach(user -> {
+            user.setStatus("update");
+            log.info("要修改的 user: [ {} ]", user);
+
+            if (!map.containsKey(user.getId())) {
+                log.warn("用户不存在！user: [ {} ]", user);
+                return;
+            }
+            user.setStatus("update");
+            map.put(user.getId(), user);
+        });
+
+        return MapBuilder.<String, Object>create()
+                .put("res", 1)
+                .put("msg", "用户更改成功")
+                .build();
+    }
+
     @DeleteMapping("user/delete/{id}")
     public Map<String, Object> deleteUser(
             @PathVariable String id
     ) {
+        log.info("delete, id: [{}]", id);
+
         if (!map.containsKey(id)) {
             return MapBuilder.<String, Object>create()
                     .put("res", 0)
@@ -102,6 +127,20 @@ public class UserController {
                 .put("res", 1)
                 .put("msg", "用户逻辑删除成功")
                 .build();
+    }
+
+    private void initData() {
+        if (map.isEmpty()) {
+            IntStream.rangeClosed(1, 3)
+                    .mapToObj(i -> new UserDto()
+                            .setId(ID_PREFIX + idAdder.incrementAndGet())
+                            .setName("test-" + i)
+                            .setAge(32 + i)
+                            .setRemark("系统创建")
+                            .setStatus("auto-new")
+                    )
+                    .forEach(dto -> map.put(dto.getId(), dto));
+        }
     }
 
 }
