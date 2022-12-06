26842b86f0550ff990a7bb29c25d39dd862f80c9
ZXFeng <fa6688@sina.com>
2022-12-06 11:40:51 +0800

    MyBatis 拦截器测试

---
+++ data/mybatis/mybatis-plugin-test/readme.md
@@ -0,0 +1,3 @@
+# MyBatis 自定义插件测试
+- 参考：https://zhuanlan.zhihu.com/p/361192268
+- 数据库 MySQL
---
+++ data/mybatis/mybatis-plugin-test/src/main/java/cn/zxf/mybatis_plugin_test/plugin/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package cn.zxf.mybatis_plugin_test.plugin;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * <br/>
+ * Created by ZXFeng on 2022/12/6.
+ */
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setPlugins(new ExecutorPlugin());
+        return sqlSessionFactoryBean.getObject();
+    }
+
+}
