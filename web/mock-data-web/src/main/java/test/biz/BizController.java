dd375698dc73f2b305697b9326aae178a86ca583
ZXFeng <fl_zxf@163.com>
2023-05-08 12:01:54 +0800

    Spring 升级

--- web/mock-data-web/src/main/java/test/biz/BizController.java
+++ web/mock-data-web/src/main/java/test/biz/BizController.java
@@ -1,11 +1,11 @@
 package test.biz;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.Map;
--- web/mock-data-web/src/main/java/test/user/LoginController.java
+++ web/mock-data-web/src/main/java/test/user/LoginController.java
@@ -1,12 +1,12 @@
 package test.user;
 
 import cn.hutool.core.map.MapBuilder;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
