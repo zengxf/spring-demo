e60dd342925fa3c808e2d05ebc503538ad6fe776
ZXFeng <fl_zxf@163.com>
2023-06-06 10:26:38 +0800

    Spring MVN 脚手架

---
+++ 0-fast-spring-starter/spring-web-starter-mvn/src/test/java/cn_zxf_test/biz/TestBizService.java
@@ -0,0 +1,21 @@
+package cn_zxf_test.biz;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.extern.slf4j.Slf4j;
+import cn_zxf_test.BaseTester;
+
+@Slf4j
+public class TestBizService extends BaseTester {
+
+    @Autowired
+    private BizService service;
+
+    @Test
+    public void hello() {
+        service.hello();
+        log.info("ok!");
+    }
+
+}
