1ce6b11e3bb7963b5e324ddc0416aabb4677a000
ZXFeng <fl_zxf@163.com>
2023-06-06 12:06:47 +0800

    Gradle Spring 3.0 测试

---
+++ feature-research/read-code/gradle-spring-3.0/src/test/java/cn/zxf/test/TestServiceTest.java
@@ -0,0 +1,22 @@
+package cn.zxf.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Slf4j
+public class TestServiceTest {
+
+    public static void main(String[] args) {
+        log.info("--------------------------------");
+
+        String path = "application.xml";
+        ApplicationContext context = new ClassPathXmlApplicationContext(path);
+
+        TestService service = (TestService) context.getBean("testService");
+        String result = service.test("admin");
+
+        log.info("result : " + result);
+    }
+
+}
