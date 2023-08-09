ed4ceb317d067db5238cec7fc44fafe2d8c1ee3f
ZXFeng <fl_zxf@163.com>
2023-08-09 11:01:22 +0800

    Spring AOP test

--- aop/aop-principle/src/main/java/cn/zxf/spring_aop/spring_order_test/Main.java
+++ aop/aop-principle/src/main/java/cn/zxf/spring_aop/spring_order_test/Main.java
@@ -1,7 +1,14 @@
 package cn.zxf.spring_aop.spring_order_test;
 
+import org.springframework.aop.aspectj.AspectJAfterAdvice;
+import org.springframework.aop.aspectj.AspectJAroundAdvice;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * 断点调试：org.springframework.aop.framework.ReflectiveMethodInvocation #proceed() 时，<br/>
+ * 会按顺序一个一个执行，只是输出有些不一致，<br/>
+ * 是因为 {@link AspectJAroundAdvice}、{@link AspectJAfterAdvice} 等拦截器内部处理会按对应逻辑进行传递，导致仅看输出时会不一致。
+ */
 public class Main {
 
     public static void main(String[] args) {
