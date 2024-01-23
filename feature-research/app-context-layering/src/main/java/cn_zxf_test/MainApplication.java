e982e8eacc2f45c72f00e51e3d231d0f18caa706
ZXFeng <fl_zxf@163.com>
2024-01-23 15:01:29 +0800

    Context 分层测试

---
+++ feature-research/app-context-layering/src/main/java/cn_zxf_test/MainApplication.java
@@ -0,0 +1,17 @@
+package cn_zxf_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p/>
+ * Created by ZXFeng on 2023/11/22
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
